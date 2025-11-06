576f2430a63e21304a83cd1d82cf78910bb762ec
incognito2-cyber <genesis6777@mail.ru>
2025-11-06 11:51:10 +0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,12 @@
-importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  messagingSenderId: "23543078001"
+  apiKey: "AIzaSyDmMKjRwOagaEdWElMy98r8shtibk9LXNM",
+  authDomain: "birthdays-reminder-4a1c1.firebaseapp.com",
+  projectId: "birthdays-reminder-4a1c1",
+  messagingSenderId: "23543078001",
+  appId: "1:23543078001:web:32ffb8edaa9b729a864fd0"
 });
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: payload.notification.icon
-  });
-});
