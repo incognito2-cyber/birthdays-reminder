f08547559f0e3287796bad98497d887274dc9213
incognito2-cyber <genesis6777@mail.ru>
2025-11-06 23:54:53 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,9 +3,8 @@ const firebaseConfig = {
   authDomain: "birthdays-reminder-4a1c1.firebaseapp.com",
   databaseURL: "https://birthdays-reminder-4a1c1-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "birthdays-reminder-4a1c1",
-  storageBucket: "birthdays-reminder-4a1c1.firebasestorage.app",
+  storageBucket: "birthdays-reminder-4a1c1.appspot.com",
   messagingSenderId: "23543078001",
-  appId: "1:23543078001:web:32ffb8edaa9b729a864fd0",
-  measurementId: "G-B9GC38FX9P"
+  appId: "1:23543078001:web:32ffb8edaa9b729a864fd0"
 };
 firebase.initializeApp(firebaseConfig);
